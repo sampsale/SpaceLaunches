6675e7eb89c224b8e8a54251d3b23cf224b1653d
Sampsa <sampsaleikas@gmail.com>
2022-04-13 00:51:09 +0300

    stuff

--- iss.js
+++ iss.js
@@ -1,29 +1,47 @@
 
 
-fetch(`http://api.open-notify.org/iss-now.json`, { mode: 'cors' })
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (responseJson) {
-        data = responseJson;
-        console.log(data)
-        initMap(data.iss_position)
-    })
-    .catch(function (error) {
-        console.log(error)
-    })
+
+let token = config.GoogleApiKey;
+
+var script = document.createElement('script');
+script.src = `https://maps.googleapis.com/maps/api/js?key=${token}&callback=fetchData`;
+script.async = true;
+
+
+function fetchData() {
+    fetch(`http://api.open-notify.org/iss-now.json`, { mode: 'cors' })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (responseJson) {
+            data = responseJson;
+            console.log(data)
+            initMap(data.iss_position)
+        })
+        .catch(function (error) {
+            console.log(error)
+        })
+
+}
 
 
 
 function initMap(location) {
     console.log(location)
-    const iss = { lat: parseInt(location.latitude), lng: parseInt(location.longitude) };
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
-        center: iss,
-    });
-    const marker = new google.maps.Marker({
-        position: iss,
-        map: map,
-    });
+    try {
+        const iss = { lat: parseInt(location.latitude), lng: parseInt(location.longitude) };
+        const map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 3,
+            center: iss,
+        });
+        const marker = new google.maps.Marker({
+            position: iss,
+            map: map,
+        });
+    } catch(err){
+        document.getElementById("map").innerHTML = "Error loading MAP, API-key might be expired or you don't have it if you got this from GitHub"
+    }
+   
 }
+
+document.head.appendChild(script);
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Site for spacelaunches 
+
+Site for space launches. You can find the next 10 space launches with countdowns, current location of ISS on Google maps, people currently in space and search function for space agencies, launch sites, launch vehicles and astronauts. Done for practice on API-usage and Bootstrap.
+
+Done with native JS and Bootstrap used for styling and mobile friendliness.
+
+APIs used: http://api.open-notify.org/astros.json http://api.open-notify.org/iss-now.json Google Maps api, https://lldev.thespacedevs.com/2.2.0/launch/upcoming/ , https://lldev.thespacedevs.com/2.2.0/agencies/?search ,  
+
+ISS location API not functional, since API-key is not on github.
+
